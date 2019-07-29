a874afde93a73a119d23a9be0148b7818054bef1
kieferm-so <53442827+kieferm-so@users.noreply.github.com>
2019-07-29 11:39:39 -0500

    Add files via upload
    Adding initial files

---
+++ tipuesearch_content.js
@@ -0,0 +1,8 @@
+
+var tipuesearch = {"pages": [
+     {"title": "SFSC | Human Brain Exhibit Interactives",
+      "text": "Catchy project title!",
+      "img": "https://static1.squarespace.com/static/5d0a74418748ef00018fb194/5d2cd4407eb4180001d448df/5d2f670528b617000121f949/1564414589229/sfsc_reaction_time_thumb.jpg?format=300w",
+      "tags": "south florida science center human brain exhibit design interactive visitor center",
+      "url": "/multiple-images-template"}
+]};
